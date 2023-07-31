36772afd8972949ab9791b964cc94da7022f7ec6
manhduonghn <manhduonghn@live.com>
2023-07-31 13:34:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-import os
+import argparse
 
 from src.build import Build
 
-# Use a dictionary to store the app names and their corresponding patches
-app_info = {
-    "youtube": {
-        "exclude_patches": "custom-branding-icon-revancify-blue,custom-branding-youtube-name",
-        "include_patches": "custom-branding-icon-revancify-red"
-    },
-    "youtube-music": {
-        "exclude_patches": ""
-        "include_patches": ""
-    },
+app_name = "youtube"
+exclude_patches = "custom-branding-icon-revancify-blue,custom-branding-youtube-name"
+include_patches = "custom-branding-icon-revancify-red"
 
-# Loop through the dictionary and build each app with its patches
-for app_name, patches in app_info.items():
-    # Create a new namespace object for each app name and its patches
-    args = argparse.Namespace(app_name=app_name, exclude_patches=patches["exclude_patches"], include_patches=patches["include_patches"])
-    build = Build(args)
-    build.run_build()
-    
+args = argparse.Namespace(app_name=app_name, exclude_patches=exclude_patches, include_patches=include_patches)
+
+build = Build(args)
+build.run_build()
