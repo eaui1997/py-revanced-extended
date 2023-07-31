93b5f2e37d8b63f099d91be1f5ff3b0f6603273f
manhduonghn <manhduonghn@live.com>
2023-08-01 05:31:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import argparse
 
 from src.build import Build
 
-app_name = "youtube"
-exclude_patches = "custom-branding-icon-revancify-blue,custom-branding-youtube-name"
-include_patches = "custom-branding-icon-revancify-red"
+app_names = ["youtube", ""]
+exclude_patches = ["custom-branding-icon-revancify-blue,custom-branding-youtube-name", ""]
+include_patches = ["custom-branding-icon-revancify-red", ""]
 
-args = argparse.Namespace(app_name=app_name, exclude_patches=exclude_patches, include_patches=include_patches)
+for i in range(len(app_names)):
+  
+    app_name = app_names[i]
+    exclude_patch = exclude_patches[i]
+    include_patch = include_patches[i]
 
-build = Build(args)
-build.run_build()
+
+    args = argparse.Namespace(app_name=app_name, exclude_patches=exclude_patch, include_patches=include_patch)
+
+    build = Build(args)
+
+    build.run_build()
+  
