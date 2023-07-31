c6e31943c090f8b566e4a995c22731af007171dd
manhduonghn <manhduonghn@live.com>
2023-07-31 07:50:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,31 +2,10 @@ import argparse
 
 from src.build import Build
 
+app_name = "youtube"
+exclude_patches = "custom-branding-icon-revancify-blue,custom-branding-youtube-name"
 
-def main():
-    parser = argparse.ArgumentParser("revanced_build")
+args = argparse.Namespace(app_name=app_name, exclude_patches=exclude_patches)
 
-    # Target build app
-    parser.add_argument("app_name", help="Target build app", type=str)
-
-    # Exclude Patches: patch1,patch2,patch3
-    parser.add_argument(
-        "--exclude-patches",
-        help="Exclude patches from build",
-        type=str,
-        default="none",
-    )
-
-    args = parser.parse_args()
-
-    build = Build(args)
-
-    build.run_build()
-
-
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("Exiting...")
-        exit(1)
+build = Build(args)
+build.run_build()
